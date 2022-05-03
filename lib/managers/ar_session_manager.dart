c4d772b8710402abab2934c364427a5e951d8c4d
Carlos Raúl Oliveros Alpírez <44912302+alpi2111@users.noreply.github.com>
2022-05-03 09:06:37 -0500

    Update ar_session_manager.dart

--- lib/managers/ar_session_manager.dart
+++ lib/managers/ar_session_manager.dart
@@ -1,5 +1,5 @@
-import 'dart:typed_data';
 import 'package:ar_flutter_plugin/models/ar_hittest_result.dart';
+import 'package:ar_flutter_plugin/utils/json_converters.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:ar_flutter_plugin/datatypes/config_planedetection.dart';
@@ -10,7 +10,7 @@ typedef ARHitResultHandler = void Function(List<ARHitTestResult> hits);
 /// Manages the session configuration, parameters and events of an [ARView]
 class ARSessionManager {
   /// Platform channel used for communication from and to [ARSessionManager]
-  late MethodChannel _channel;
+  MethodChannel _channel;
 
   /// Debugging status flag. If true, all platform calls are printed. Defaults to false.
   final bool debug;
@@ -22,7 +22,7 @@ class ARSessionManager {
   final PlaneDetectionConfig planeDetectionConfig;
 
   /// Receives hit results from user taps with tracked planes or feature points
-  late ARHitResultHandler onPlaneOrPointTap;
+  ARHitResultHandler onPlaneOrPointTap;
 
   ARSessionManager(int id, this.buildContext, this.planeDetectionConfig,
       {this.debug = false}) {
@@ -58,16 +58,13 @@ class ARSessionManager {
             onPlaneOrPointTap(hitTestResults);
           }
           break;
-        case 'dispose':
-          _channel.invokeMethod<void>("dispose");
-          break;
         default:
           if (debug) {
             print('Unimplemented method ${call.method} ');
           }
       }
     } catch (e) {
-      print('Error caught: ' + e.toString());
+      print('Error caught: ' + e);
     }
     return Future.value();
   }
@@ -76,25 +73,19 @@ class ARSessionManager {
   /// [customPlaneTexturePath] refers to flutter assets from the app that is calling this function, NOT to assets within this plugin. Make sure
   /// the assets are correctly registered in the pubspec.yaml of the parent app (e.g. the ./example app in this plugin's repo)
   onInitialize({
-    bool showAnimatedGuide = true,
     bool showFeaturePoints = false,
     bool showPlanes = true,
-    String? customPlaneTexturePath,
+    String customPlaneTexturePath,
     bool showWorldOrigin = false,
     bool handleTaps = true,
-    bool handlePans = false, // nodes are not draggable by default
-    bool handleRotation = false, // nodes can not be rotated by default
   }) {
     _channel.invokeMethod<void>('init', {
-      'showAnimatedGuide': showAnimatedGuide,
       'showFeaturePoints': showFeaturePoints,
       'planeDetectionConfig': planeDetectionConfig.index,
       'showPlanes': showPlanes,
       'customPlaneTexturePath': customPlaneTexturePath,
       'showWorldOrigin': showWorldOrigin,
       'handleTaps': handleTaps,
-      'handlePans': handlePans,
-      'handleRotation': handleRotation,
     });
   }
 
@@ -108,19 +99,15 @@ class ARSessionManager {
                 ScaffoldMessenger.of(buildContext).hideCurrentSnackBar)));
   }
 
-  /// Dispose the AR view on the platforms to pause the scenes and disconnect the platform handlers.
-  /// You should call this before removing the AR view to prevent out of memory erros
-  dispose() async {
+  /// Returns the camera pose with respect to the world coordinate system of the [ARView]
+  Future<Matrix4> getCameraPose() async {
     try {
-      await _channel.invokeMethod<void>("dispose");
+      final serializedCameraPose =
+          await _channel.invokeMethod<List<dynamic>>('getCameraPose', {});
+      return MatrixConverter().fromJson(serializedCameraPose);
     } catch (e) {
-      print(e);
+      print('Error caught: ' + e);
+      return null;
     }
-  }
-
-  /// Returns a future ImageProvider that contains a screenshot of the current AR Scene
-  Future<ImageProvider> snapshot() async {
-    final result = await _channel.invokeMethod<Uint8List>('snapshot');
-    return MemoryImage(result!);
   }
 }
